f819e981e17d73e446782329299536d92bf7cbf2
gabrielrodriguesrocha <gabrielrodrigues_rocha@hotmail.com>
2019-06-06 01:37:50 -0300

    Added character n-grams and some results

--- util.py
+++ util.py
@@ -3,8 +3,11 @@
 
 import numpy as np
 import pandas as pd
+from nltk.stem.porter import *
+from nltk.corpus import stopwords
 from stemmer import Stemmer
 import re,string
+from itertools import chain
 
 ###########################
 #                         #
@@ -13,57 +16,58 @@ import re,string
 ###########################
 
 def tweet_cleaner(tweet, stopwords = None, s = None):
-    if stopwords is None:
-        stopwords = pd.read_csv( 'datasets/stopwords_nltk.csv', sep=',', index_col=None, header=None)
-        stopwords =  np.concatenate((stopwords.iloc[:,0].values, ['AT_USER', 'URL']))
-    if s is None:
-        s = Stemmer()
     tweet = tweet.lower() # convert text to lower-case
     tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet) # remove URLs
     tweet = re.sub('@[^\s]+', 'AT_USER', tweet) # remove usernames
     tweet = re.sub(r'#([^\s]+)', r'\1', tweet) # remove the # in #hashtag
     tweet = tweet.translate(str.maketrans(string.punctuation,len(string.punctuation)*' ')) # remove punctuation
     tweet = re.findall(r'[a-z]+', tweet) # split words
-    tweet = [s.stem(word) for word in tweet] # stemming words
-    tweet = [word for word in tweet if word not in stopwords] # removing stopwords
+    if s is not None:
+        tweet = [s.stem(word) for word in tweet] # stemming words
+    if stopwords is not None:
+        tweet = [word for word in tweet if word not in stopwords] # removing stopwords
     return tweet
 
 def tweets_cleaner(tweets, stopwords = None, s = None):
     cleaned_tweets = []
-    if stopwords is None:
-        stopwords = pd.read_csv( 'datasets/stopwords_nltk.csv', sep=',', index_col=None, header=None)
-        stopwords =  np.concatenate((stopwords.iloc[:,0].values, ['AT_USER', 'URL']))
-    if s is None:
-        s = Stemmer()
     for i in tweets:
         cleaned_tweets.append(tweet_cleaner(i, stopwords, s))
     return cleaned_tweets
 
-def build_vocab(cleaned_tweets):
-    vocab = []
+def ngrams(word, n):
+    return [word[i:i+n] for i in range(len(word)-1)]
+
+def build_vocab(cleaned_tweets, n = 0):
+    vocab = np.array([])
     
     for i in cleaned_tweets:
-        vocab.extend(i)
+        if n == 0:
+            vocab = np.append(vocab, i)
+        else:
+            vocab = np.append(vocab, list(chain.from_iterable([ngrams(word,n) for word in i])))
         
-    vocab = np.sort(list(set(vocab)))
+    print(vocab[:5])
+    vocab = np.unique(vocab.flatten())
     
     return vocab
 
-def build_features(tweet, vocab):
+def build_features(tweet, vocab, rep = 'bow', n = 0):
     features = np.zeros(len(vocab))
+    if rep == 'ngrams':
+        tweet = list(chain.from_iterable([ngrams(word,n) for word in tweet]))
     unique, counts = np.unique(tweet, return_counts=True)
     features[np.searchsorted(vocab, unique).astype(int)] = counts
     #np.insert(features,np.searchsorted(vocab,unique),counts)
     #features = np.isin(vocab, tweet).astype(int)
     return features.astype(int)
 
-def build_representation(tweets, vocab):
+def build_representation(tweets, vocab, rep = 'bow', n = 0):
     data = []
     for idx, i in enumerate(tweets):
-        data.append(build_features(i,vocab))
+        data.append(build_features(i,vocab, rep, n))
     return data
 
-def extract_features(dataset):
+def extract_features(dataset, rep = 'bow', n=0):
     df_dataset = pd.read_csv( dataset, sep=',', index_col=None, header=0)
 
     X = df_dataset['TWEET'].values
@@ -73,11 +77,11 @@ def extract_features(dataset):
     Y = np.where(Y != 1, Y, 1)
     
     #limpa e tokeniza os tweets
-    cleaned_tweets = tweets_cleaner(X)
+    cleaned_tweets = tweets_cleaner(X, stopwords = stopwords.words('english'), s = PorterStemmer())
     #constrói o vocabulário
-    vocab = build_vocab(cleaned_tweets)
+    vocab = build_vocab(cleaned_tweets, n)
     #constrói o vetor de features para cada tweet
-    Xfeatures = np.array(build_representation(cleaned_tweets, vocab))
+    Xfeatures = np.array(build_representation(cleaned_tweets,vocab,rep,n))
     #f = util.build_features(X[1067], vocab)
     #print(Y.shape)
     
